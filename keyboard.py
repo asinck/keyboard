653462f106e03ebc0ea646a9bf07fcd99f105044
Adam Sinck <absinck+github@grueslayer.com>
2015-10-02 22:37:06 -0700

    Updating the files

--- keyboard.py
+++ keyboard.py
@@ -1,3 +1,8 @@
+#This program allows the user to "type" characters that are not on a
+#standard keyboard.
+
+
+
 from Tkinter import *
 import Tkinter as tk
 import time
@@ -47,38 +52,34 @@ def change(newAlphabet):
         for i in kbd:
             i.pack_forget()
 
+    #letterOrder is an array of the letters, so that I can define the order
+    letterOrder = []
+    #letterSet is a hash table of the new letters
+    letterSet = {}
+
     letters = {}
-    letterset = []
-    caps = {}
-    lower = {}
+    caps = True #whether or not the set of characters has a capitalization
     kbd = []
     if newAlphabet == "greek":
-        letterset = greekLetters
-        caps = greekCLetters
-        lower = greekLLetters
+        letterOrder = greek
+        letters = greekLetters
 
     elif newAlphabet == "english":
-        letterset = englishLetters
-        caps = englishCLetters
-        lower = englishLLetters
+        letterOrder = english
+        letters = englishLetters
 
     elif newAlphabet == "symbols":
-        letterset = symbols
-        caps = symbolLetters
-        lower = symbolLetters
+        letterOrder = symbols
+        letters = symbolLetters
 
     elif newAlphabet == "circles":
-        letterset = circles
-        caps = circledCLetters
-        lower = circledLLetters
-        
-    for i in letterset:
-        #give keys both upper and lower, and toggle if [0] or [1] is
-        #displayed/used by using shift
-        letters[i] = [caps[i], lower[i]]
+        letterOrder = circles
+        letters = circledLetters
 
-
+    if len(letters[letterOrder[0]]) == 1:
+        caps = False
     
+    #some specific formatting for certain character sets
     height = 3
     width = (len(letters) / height) + 1
     if alphabet == "english" or newAlphabet == "circles":
@@ -87,22 +88,22 @@ def change(newAlphabet):
     elif alphabet == "symbols":
         width = 11
         height = len(letters) / 11 + 1
+
     rows = 0
     count = 0
-#    f = Frame(keyboardFrame)
-#    kbd.append(f.pack(side=TOP))
-#    print f, kbd
     buttons = []
+
     for i in range(height):
         kbd.append(Frame(keyboardFrame))
         kbd[i].pack(side=TOP)
-    for letter in letterset:
+    for letter in letterOrder:
         if count == width:
             count = 0
             rows += 1
-        if shiftDown:
-            myText = letters[letter][0]
-        else:
+        #capital letters are the first entry in the pair
+        myText = letters[letter][0]
+        #this should only put lowercase letters if there are any
+        if caps and not shiftDown:
             myText = letters[letter][1]
         myCommand =  lambda letter = letter: typeLetter(letters[letter])
         b = Button(kbd[rows], text = myText, command = myCommand)
@@ -114,7 +115,7 @@ def change(newAlphabet):
 
 root = Tk()
 root.title("UTF-8 Keyboard")
-icon = PhotoImage(file="joystick.png")
+icon = PhotoImage(file="icon.png")
 root.tk.call('wm', 'iconphoto', root._w, icon)
 
 leftFrame = Frame(root)
@@ -125,10 +126,10 @@ output = Text(textFrame, height = 3, width = 70)
 
 shift = Button(selectionFrame, text = u'\u2193 Shift \u2193', command = lambda: toggleShift())
 
-greekButton = Button(selectionFrame, text = "Greek", command = lambda: change("greek"))
-englishButton = Button(selectionFrame, text = "English", command = lambda: change("english"))
-symbolButton = Button(selectionFrame, text = "Symbols", command = lambda: change("symbols"))
-circlesButton = Button(selectionFrame, text = "Circles", command = lambda: change("circles"))
+greekButton=Button(selectionFrame,text="Greek",command=lambda:change("greek"))
+englishButton=Button(selectionFrame,text="English",command=lambda:change("english"))
+symbolButton=Button(selectionFrame,text="Symbols",command=lambda:change("symbols"))
+circlesButton=Button(selectionFrame,text="Circles",command=lambda:change("circles"))
 clear = Button(selectionFrame, text = "Clear", command = lambda: clearToPoint())
 
 leftFrame.pack(side=LEFT)
